88b5fa3fb6408b2d0cde06c549239fa8ada77215
charlyls <carlosgbozzola@hotmail.com>
2022-09-01 20:24:29 -0300

    delete alumnos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Intro from "./Components/Intro/Intro";
 import Cursos from "./Components/Cursos/Cursos";
-import Alumnos from "./Components/Alumnos/Alumnos";
 
 function App() {
   return (
