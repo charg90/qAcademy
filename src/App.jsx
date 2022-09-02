e46dd2ab9a7b2d92c37ae2d9b7b37fa11f329c9c
charlyls <carlosgbozzola@hotmail.com>
2022-09-02 19:24:45 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Intro from "./Components/Intro/Intro";
 import Target from "./Components/Target/Target";
 import Alumnos from "./Components/Alumnos/Alumnos";
-import Cursos from "./Components/Cursos/Cursos";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
