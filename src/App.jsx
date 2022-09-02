906596b11b6ee131462cd79c41376aeeac811d98
charlyls <carlosgbozzola@hotmail.com>
2022-09-02 19:27:58 -0300

    add cursos

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Intro from "./Components/Intro/Intro";
 import Target from "./Components/Target/Target";
 import Alumnos from "./Components/Alumnos/Alumnos";
 import Footer from "./Components/Footer/Footer";
-
+import Cursos from "./Components/Cursos/Cursos";
 function App() {
   return (
     <>
       <Intro />
       <Target />
       <Alumnos />
+      <Cursos />
 
       <Footer />
     </>
