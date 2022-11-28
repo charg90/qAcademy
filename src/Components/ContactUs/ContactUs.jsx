b827689287eb5427ca85d41590a1538e20891c15
charlyls <carlosgbozzola@hotmail.com>
2022-11-28 19:18:52 -0300

    fix email sending

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -9,7 +9,8 @@ import styles from "./contactus.module.css";
 const ContactUs = () => {
   const form = useRef();
   const sendEmail = (data) => {
-    console.log(data);
+    console.log(form.current);
+
     emailjs
       .sendForm(
         "service_f3uv7pg",
@@ -59,18 +60,20 @@ const ContactUs = () => {
       fuild
       className={`${styles.container} min-vh-100 d-flex  align-items-center justify-content-center  `}
     >
-      <Col
-        md={12}
-        className="d-flex flex-column justify-content-center align-items-center"
-      >
-        <h2 className="mb-4"> Contacto </h2>
+      <Col sm={12} md={6} lg={6} className=" ">
+        <h2 className="mb-4 text-center"> Contacto </h2>
         <Form
           onSubmit={handleSubmit(sendEmail)}
-          className={`${styles.formContact} p-4 xs-w-75 w-75`}
+          className={`${styles.formContact} p-4  `}
           ref={form}
         >
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label className={`${styles.formLabels}`}>
+          <Form.Group
+            className="mb-3 d-flex flex-column align-items-center"
+            controlId="formBasicEmail"
+          >
+            <Form.Label
+              className={`${styles.formLabels} justify-content-center `}
+            >
               Nombre y Apellido*{" "}
             </Form.Label>
             <Form.Control
@@ -82,24 +85,27 @@ const ContactUs = () => {
             />
           </Form.Group>
           {errors.nombreCompleto && (
-            <p className={`${styles.formErrors}`}>
+            <p className={`${styles.formErrors} text-center`}>
               {errors.nombreCompleto.message.message}
             </p>
           )}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group
+            className="mb-3 d-flex flex-column align-items-center   "
+            controlId="formBasicEmail"
+          >
             <Form.Label className={`${styles.formLabels}`}>Email* </Form.Label>
             <Form.Control
               type="text"
               placeholder="qcapital@example.com"
               {...register("email")}
-              className={`${styles.formInputs}`}
+              className={`${styles.formInputs}   `}
               name="email"
             />
           </Form.Group>
-          <p className={`${styles.formErrors}`}>
+          <p className={`${styles.formErrors} text-center`}>
             {errors.email && errors.email.message.message}
           </p>
-          <Form.Label className={`${styles.formLabels}`}>
+          <Form.Label className={`${styles.formLabels} text-center`}>
             Charlemos*{" "}
           </Form.Label>
           <FloatingLabel controlId="floatingTextarea2">
@@ -111,7 +117,7 @@ const ContactUs = () => {
               name="comentario"
             />
           </FloatingLabel>
-          <p className={`${styles.formErrors}`}>
+          <p className={`${styles.formErrors} text-center`}>
             {errors.comentarios && errors.comentarios.message.message}
           </p>
           <Button className="mt-2" variant="primary" type="submit">
