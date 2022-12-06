8a2a1f8f4dca1947b2e74d614017462215080434
charlyls <carlosgbozzola@hotmail.com>
2022-12-06 20:12:43 -0300

    add new modals

---
+++ src/Components/Modals/ModalTraderInv/ModalTraderInver.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import styles from "./modalTraderInver.module.css";
+export const ModalTraderInver = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button className={`${styles.btnVerMas}`} onClick={handleShow}>
+        Ver Mas
+      </button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className={`${styles.modal} `}
+      >
+        <Modal.Header className={`${styles.modalHeader} `} closeButton>
+          <Modal.Title className="text-center">
+            Combo Trader/Inversor Bursatil:
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={`${styles.modalBody} `}>
+          Combina los dos mundos mas importantes de los mercados financieros. No
+          es necesario que cuentes con experiencia previa. Esto te dara la
+          posibilidad de una polivalencia mucho mas amplia y obviamente contaras
+          con un beneficio no solo en relacion a lo economico en cuanto a los
+          costos del curso, sino tambien que contaras con bonos adicionales que
+          haciendo los modulos por separado no podras obtener. Estos bonos,
+          estaran relacionados en Cryptomercados, Herramientas de resolucion
+          QRTI (Sistema para resolucion de problemas avanzados), Psicotrading,
+          Financiamiento en los mercados para Pequeñas y medianas empresas
+          (Pymes). Este ultimo es un bono especial para todos aquellos
+          empresarios que necesitan de nuevas opciones de financiamiento, que
+          podran obtener dentro del mundo bursatil con el respaldo de
+          instituciones que le otorgaran una mejor posicion economica en corto y
+          largo plazo.
+        </Modal.Body>
+        <Modal.Footer className={`${styles.modalFooter}`}>
+          <button className={`${styles.btnModal}`} onClick={handleClose}>
+            Cerrar
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+export default ModalTraderInver;
