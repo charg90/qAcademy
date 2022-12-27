5acccd87c22c888f30d5bc404b7597aa62f88a8f
charlyls <carlosgbozzola@hotmail.com>
2022-12-27 19:34:09 -0300

    delete reg email file

--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -4,7 +4,7 @@ import styles from "./registration.module.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { RegistrationSchema } from "../Schemas/RegistrationSchema";
-import { registrationWithEmail } from "../../FireBase/RegistrationWithEmail";
+// import { registrationWithEmail } from "../../FireBase/RegistrationWithEmail";
 import { signInWithPopupGoogle } from "../../FireBase/registrationGoogle";
 import { registrationFacebook } from "../../FireBase/registrationFacebook";
 import { useNavigate } from "react-router-dom";
@@ -25,7 +25,7 @@ export const Registration = () => {
     resolver: yupResolver(RegistrationSchema),
   });
   const onSubmit = async (data) => {
-    const response = await registrationWithEmail(data.email, data.password);
+    // const response = await registrationWithEmail(data.email, data.password);
     dispatch(logIn(response));
     navigate("/userHome");
   };
