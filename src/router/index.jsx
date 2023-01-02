eccdc0578b4347a703e1076b03bf5e005e32fb4d
charlyls <carlosgbozzola@hotmail.com>
2023-01-02 13:36:03 -0300

    change to hashRouter

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import LayoutPublic from "../Layout/LayoutPublic";
 import LayoutUserHome from "../Layout/LayoutUserHome";
 import Home from "../Pages/Home/Home";
@@ -6,7 +6,7 @@ import UserHome from "../Pages/UserHome/UserHome";
 import LoginRegistration from "../Pages/LoginRegistration/LoginRegistration";
 import ProtectedRoutes from "./../Components/Protected Routes/ProtectedRoute";
 import Admin from "../Components/Admin/Admin";
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <LayoutPublic />,
